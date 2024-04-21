dc042343c8d620670f6ac97af64c4a28435e592b
Ivan Pronin <pacernestit@rambler.ru>
2024-04-21 22:03:51 +0300

    dev feat (подключил редакс, добавил первый редюсер)

---
+++ src/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from "@reduxjs/toolkit";
+import counterSlice from "./store/counterSlice";
+
+export const store = configureStore({
+  reducer: {
+    counter: counterSlice,
+  },
+  devTools: true,
+});
